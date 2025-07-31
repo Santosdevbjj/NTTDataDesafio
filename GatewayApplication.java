2a343b3f4fb33c55436380cd50a25ddffcfa3a67
Sergio <santossergiorealbjj@outlook.com>
2025-07-31 17:52:21 -0300

    Create GatewayApplication.java

---
+++ GatewayApplication.java
@@ -0,0 +1,5 @@
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+  public static void main(String[] args){ SpringApplication.run(GatewayApplication.class,args); }
+}
